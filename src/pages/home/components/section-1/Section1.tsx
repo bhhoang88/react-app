d0d9991e816a2e8515dcb539567d14d98b0a689f
roosterruler <88428487+bhhoang818@users.noreply.github.com>
2024-02-25 01:41:55 +0700

    add

--- src/pages/home/components/section-1/Section1.tsx
+++ src/pages/home/components/section-1/Section1.tsx
@@ -35,18 +35,18 @@ const Section1 = () => {
             <div className="container">
                 <div className="grid grid-cols-12 gap-6">
                     {brands.map((brand, index) => (
-                        <div key={index} className="col-span-6">
+                        <div key={index} className="col-span-full md:col-span-6">
                             <Link to={brand.url} className='brand-item flex items-center gap-6 zoom-img'>
                                 <picture className='img flex-auto w-1/2'>
                                     <source srcSet={brand.image} type="image/png" />
                                     <img src={brand.image} alt="" />
                                 </picture>
-                                <div className="brand-infor flex flex-col items-center justify-center flex-auto w-1/2 gap-4">
+                                <div className="brand-infor flex flex-col items-center flex-auto w-1/2 gap-4">
                                     <h2 className='text-2xl font-semibold text-black'>{brand.name}</h2>
-                                    <p className='text-sm font-light text-center'>
+                                    <p className='text-sm font-light text-center mb-auto'>
                                         {brand.description}
                                     </p>
-                                    <Button placeholder={undefined}>Xem Thêm</Button>
+                                    <Button placeholder={undefined} className='btn btn-view-more'>Xem Thêm</Button>
                                 </div>
                             </Link>
                         </div>
